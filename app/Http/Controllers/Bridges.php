3a8f94537b5970e2f915d447f8169405a41edf6e
Ethan S <55769721+Ethansteip@users.noreply.github.com>
2020-11-10 19:19:07 -0500

    Update Bridges.php

--- app/Http/Controllers/Bridges.php
+++ app/Http/Controllers/Bridges.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class Bridges extends Controller
 {
+    //Find the first instance of bridgeA's status switching from "Available" to "Raising Soon".
+
+        //Save the time stamp of that instance
+
+        //Find the next instance of BridgeA's status switching from "Raising Soon" to "Fully Raised".
+
+        //Save that timestamp.
+        
+        //Find the duration between those 2 timestamps.
     
 }
