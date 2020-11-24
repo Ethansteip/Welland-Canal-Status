114bf176bc2bd8c0cab6e29b32e06848c87769c5
Ethan S <55769721+Ethansteip@users.noreply.github.com>
2020-11-24 16:01:06 -0500

    Moved insertToDB files to services

--- database/factories/TestFactory.php
+++ database/factories/TestFactory.php
@@ -24,7 +24,7 @@ class TestFactory extends Factory
         return [
             
             'bridge_name' => $this->faker->randomElement(['Bridge 1', 'Bridge 2', 'Bridge 3', 'Bridge 4', 'bridge 5', 'Bridge 6', 'Bridge 7']),
-            'bridge_status' => $this->faker->randompElement(['Available', 'Raising', 'Lowering', 'Fully Raised', 'Under Contruction']),
+            'bridge_status' => $this->faker->randomElement(['Available', 'Raising', 'Lowering', 'Fully Raised', 'Under Contruction']),
             'bridge_id' => $this->faker->randomElement(['1', '2', '3', '4', '5', '6', '7']),
             'status_time' => $this->faker->unixTime($max = 'now'),
 
